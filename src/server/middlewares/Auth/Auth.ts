c1507266d65e0b11ef61bba331df40d3b020e86f
oriolraventosc <oriol.raventos.c@gmail.com>
2023-02-02 08:56:39 +0100

    Create Auth middleware and it's test

---
+++ src/server/middlewares/Auth/Auth.ts
@@ -0,0 +1,43 @@
+import type { CustomRequest, UserTokenPayload } from "../../../types/types.js";
+import jwt from "jsonwebtoken";
+import type { Response, NextFunction } from "express";
+import CustomError from "../../customError/customError.js";
+import enviroment from "../../../loadEnviroment.js";
+
+const auth = (req: CustomRequest, res: Response, next: NextFunction) => {
+  try {
+    const authorizationHeader: string = req.header("Authorization");
+
+    if (!authorizationHeader) {
+      const customError = new CustomError(
+        "Authorization missing",
+        401,
+        "Authorization missing"
+      );
+      next(customError);
+      return;
+    }
+
+    if (!authorizationHeader.startsWith("Bearer ")) {
+      const customError = new CustomError(
+        "Missing Bearer",
+        401,
+        "Missing Bearer"
+      );
+      next(customError);
+    }
+
+    const token: string = authorizationHeader.replace(/^Bearer\s*/, "");
+
+    const user = jwt.verify(token, enviroment.jwtSecretKey) as UserTokenPayload;
+
+    req.userId = user.id;
+
+    next();
+  } catch {
+    const customError = new CustomError("Invalid token", 500, "Invalid token");
+    next(customError);
+  }
+};
+
+export default auth;
